ec7b042ac08689852ad9b66d0c2a13cc0c9da79f
Kate <katepdonahue@gmail.com>
2013-12-11 11:14:01 -0500

    Fix duplicate artists bug by adding a downcase in Artist#find_by_name method

--- lib/artist.rb
+++ lib/artist.rb
@@ -39,7 +39,7 @@ class Artist
   end
 
   def self.find_by_name(string)
-    @@artists.detect{|a| a.name.downcase == string}
+    @@artists.detect{|a| a.name.downcase == string.downcase}
   end
 
   def url
